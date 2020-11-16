38932ec76f0f874e0120c2ea32d5e131bf7843b3
MarceloLuisDantas <marceloluis257gh@gmail.com>
2020-11-16 12:26:36 -0300

    Adicionado procura de genero

---
+++ src/genero.py
@@ -0,0 +1,22 @@
+import wikipedia
+
+bandas = ["Unlucky Morpheus", "Angra", "Turisas", "Wind Rose", "Dark Moor"]
+
+def getGenero(descricao: [str]) -> str :
+    for i, v in enumerate(descricao):
+        if v.lower() == "metal" :
+            return descricao[i - 1].capitalize() + " " + v.capitalize()
+    return "Metal"
+
+def procura(banda: str) -> [str] :
+    try :
+        return wikipedia.summary(banda + " (band)").split(" ")
+    except :
+        try :
+            return wikipedia.summary(banda).split(" ")
+        except :
+            return [" ", "Metal"]
+
+for banda in bandas :
+    genero = getGenero(procura(banda))
+    print(banda + " - " + genero)
